d04481bbad79a3fe246a2034846253ea61605975
Vladimir Hambardzumyan <vladimir.hambardzumyan@polymorphic.io>
2023-01-20 17:09:21 +0400

    feat(TextConstants): add project texts in constants

---
+++ src/constants/TextConstants.ts
@@ -0,0 +1,27 @@
+const TextConstants = {
+  BUTTONS: {
+    SIGN_IN: 'Sign in',
+    SIGN_UP: 'Sign up',
+    SIGN_OUT: 'Sign out'
+  },
+
+  SIGN_UP: {
+    TITLE: 'Register',
+    SUB_TITLE: 'Already have an account? '
+  },
+
+  SIGN_IN: {
+    TITLE: 'Log in to your account',
+    SUB_TITLE: "Don't have an account? "
+  },
+  ERROR_TEXT: {
+    PASSWORD:
+      'Password must be at least 8 characters that include at least 1 lowercase letter, 1 uppercase letter, 1 number, and 1 special character.',
+    EMAIL: 'Please enter a valid email address.',
+    REQUIRED: 'This field is required.'
+  },
+
+  BEE_WEB: 'Bee Web'
+}
+
+export default TextConstants
