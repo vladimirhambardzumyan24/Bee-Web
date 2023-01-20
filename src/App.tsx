1e662ebb97e14d46395f9f7150a8d09e60b89fdd
Vladimir Hambardzumyan <vladimir.hambardzumyan@polymorphic.io>
2023-01-20 17:06:17 +0400

    feat(App): create routes and ubdate App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from './components/Dashboard'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SignIn from './components/authentication/SignIn'
+import SignUp from './components/authentication/SignUp'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
