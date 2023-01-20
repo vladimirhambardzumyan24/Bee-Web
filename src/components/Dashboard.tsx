cdfec92963d180464b8f66c8ba1e12023426b0a9
Vladimir Hambardzumyan <vladimir.hambardzumyan@polymorphic.io>
2023-01-20 17:07:08 +0400

    feat(Dashboard): create Dashboard component

---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,35 @@
+import { Container } from '@mui/material'
+import { Box } from '@mui/system'
+import CardItem from './CardItem'
+import Header from './header/Header'
+
+const Dashboard = () => {
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Box
+          component="div"
+          sx={{
+            display: 'flex',
+            flexWrap: 'wrap',
+            gap: '20px',
+            justifyContent: 'center'
+          }}
+        >
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardItem />
+          <CardItem />
+        </Box>
+      </Container>
+    </>
+  )
+}
+
+export default Dashboard
