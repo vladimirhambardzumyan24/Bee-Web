16d6b66a53aba11ad52e08800f2ff8ea67feb249
Vladimir Hambardzumyan <vladimir.hambardzumyan@polymorphic.io>
2023-01-21 01:47:22 +0400

    feat(Editor): add delete button and fix text block ui issues

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -3,8 +3,15 @@ import { Slate, Editable, withReact } from 'slate-react'
 import { useMemo, useState } from 'react'
 import { createEditor, Descendant } from 'slate'
 import { BlockDataType } from '../global-components/GlobalTypes'
+import { Box } from '@mui/material'
 
-const Editor = ({ blockData }: { blockData: BlockDataType }) => {
+const Editor = ({
+  blockData,
+  handleDeleteBlock
+}: {
+  blockData: BlockDataType
+  handleDeleteBlock: (id: string) => void
+}) => {
   const editor = useMemo(() => withReact(createEditor()), [])
   const [value, setValue] = useState<Descendant[]>([
     {
@@ -13,11 +20,36 @@ const Editor = ({ blockData }: { blockData: BlockDataType }) => {
   ])
 
   return (
-    <Card sx={{ width: 275, height: 100, padding: '10px' }}>
-      <Slate editor={editor} value={value} onChange={v => setValue(v)}>
-        <Editable />
-      </Slate>
-    </Card>
+    <Box
+      component="div"
+      sx={{
+        position: 'relative'
+      }}
+    >
+      <Card
+        sx={{
+          width: 275,
+          height: 100,
+          padding: '10px 10px 20px 10px'
+        }}
+      >
+        <Slate editor={editor} value={value} onChange={v => setValue(v)}>
+          <Editable />
+        </Slate>
+      </Card>
+      <Box
+        sx={{
+          right: '10px',
+          top: '5px',
+          position: 'absolute',
+          cursor: 'pointer',
+          color: 'red'
+        }}
+        onClick={() => handleDeleteBlock(blockData.id)}
+      >
+        X
+      </Box>
+    </Box>
   )
 }
 
