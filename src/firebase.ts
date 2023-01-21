ca6c67402cd1b42116ededfa04810af5b5e32d8b
Vladimir Hambardzumyan <vladimir.hambardzumyan@polymorphic.io>
2023-01-21 13:40:59 +0400

    fix(firebase): read firebaseConfig values from env

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,13 +5,13 @@ import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCOlJnMcOmZHhYpHNQYYpzJiEGqPhwnhUU',
-  authDomain: 'bee-web-5fe17.firebaseapp.com',
-  databaseURL: 'https://bee-web-5fe17-default-rtdb.firebaseio.com',
-  projectId: 'bee-web-5fe17',
-  storageBucket: 'bee-web-5fe17.appspot.com',
-  messagingSenderId: '651837342955',
-  appId: '1:651837342955:web:e7780ae1170719b1b2aaae'
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID
 }
 
 // Initialize Firebase
