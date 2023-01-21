b342e0a580364bdf7b36a10551b2f917e2d5b176
Vladimir Hambardzumyan <vladimir.hambardzumyan@polymorphic.io>
2023-01-21 19:53:37 +0400

    feat(Editor):  add placeholder

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -4,6 +4,7 @@ import { createEditor, Descendant } from 'slate'
 import Card from '@mui/material/Card'
 import { Box } from '@mui/material'
 import { BlockDataType } from '../global-components/GlobalTypes'
+import TextConstants from '../constants/TextConstants'
 
 const Editor = ({
   blockData,
@@ -38,13 +39,12 @@ const Editor = ({
     >
       <Card
         sx={{
-          width: 275,
           height: 100,
           padding: '10px 10px 20px 10px'
         }}
       >
         <Slate editor={editor} value={value} onChange={handleChange}>
-          <Editable />
+          <Editable placeholder={TextConstants.BLOCK_PLACEHOLDER} />
         </Slate>
       </Card>
       <Box
